246653e39f85979bd1e6eb99bcec5d589d060e79
Hugh Guiney <hugh@hughguiney.com>
2020-08-31 21:50:36 -0400

    Add Web Components config

---
+++ web-components.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": [
+    "hughx",
+  ],
+  "env": {
+    "browser": true,
+  },
+  "parserOptions": {
+    "sourceType": "module",
+  },
+  "rules": {
+    "import/extensions": [
+      "error", "always"
+    ]
+  }
+};
